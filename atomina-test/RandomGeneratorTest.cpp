f825981db55782f054073f232eabe364a139e856
Nate60 <natealdred60@gmail.com>
2021-05-17 11:29:12 -0400

    PA-6: Random Generator

---
+++ atomina-test/RandomGeneratorTest.cpp
@@ -0,0 +1,79 @@
+#include "CppUnitTest.h"
+#include <atomina.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Random {
+
+	TEST_CLASS(Random_Generator) {
+
+		TEST_METHOD(Random_positive_int) {
+			ATMA::RandomGenerator<int> rg;
+			int value = rg(0, 10);
+
+			Assert::IsTrue(value >= 0 && value <= 10);
+		}
+
+		TEST_METHOD(Random_negative_int) {
+			ATMA::RandomGenerator<int> rg;
+			int value = rg(-10, 0);
+
+			Assert::IsTrue(value <= 0 && value >= -10);
+		}
+
+
+		TEST_METHOD(Random_mixed_int) {
+			ATMA::RandomGenerator<int> rg;
+			int value = rg(-10, 10);
+
+			Assert::IsTrue(value >= -10 && value <= 10);
+		}
+
+		TEST_METHOD(Random_positive_float) {
+			ATMA::RandomGenerator<float> rg;
+			int value = rg(0.0f, 10.0f);
+
+			Assert::IsTrue(value >= 0.0f && value <= 10.0f);
+		}
+
+		TEST_METHOD(Random_negative_float) {
+			ATMA::RandomGenerator<float> rg;
+			int value = rg(-10.0f, 0.0f);
+
+			Assert::IsTrue(value <= 0.0f && value >= -10.0f);
+		}
+
+
+		TEST_METHOD(Random_mixed_float) {
+			ATMA::RandomGenerator<float> rg;
+			int value = rg(-10.0f, 10.0f);
+
+			Assert::IsTrue(value >= -10.0f && value <= 10.0f);
+		}
+
+
+		TEST_METHOD(Random_positive_double) {
+			ATMA::RandomGenerator<double> rg;
+			int value = rg(0.0, 10.0);
+
+			Assert::IsTrue(value >= 0.0 && value <= 10.0);
+		}
+
+		TEST_METHOD(Random_negative_double) {
+			ATMA::RandomGenerator<double> rg;
+			int value = rg(-10.0, 0.0);
+
+			Assert::IsTrue(value <= 0.0 && value >= -10.0);
+		}
+
+
+		TEST_METHOD(Random_mixed_double) {
+			ATMA::RandomGenerator<double> rg;
+			int value = rg(-10.0, 10.0);
+
+			Assert::IsTrue(value >= -10.0 && value <= 10.0);
+		}
+
+	};
+
+}
