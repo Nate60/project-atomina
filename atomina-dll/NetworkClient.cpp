39e4f25ce0eed5c55422c552a45b8c3a149285bd
Nate60 <natealdred60@gmail.com>
2022-02-26 11:56:37 -0500

    ATMA-10: Added SFML as submodule

---
+++ atomina-dll/NetworkClient.cpp
@@ -0,0 +1,42 @@
+#include "pch.h"
+#include "NetworkClient.h"
+
+namespace ATMA {
+
+	//NetworkClient::NetworkClient(const std::string l_addr, const int l_port) : _addr(l_addr), _port(l_port)
+	//{
+
+	//}
+
+	//NetworkClient::NetworkClient(const NetworkClient& l_other)
+	//{
+	//	_addr = l_other._addr;
+	//	_port = l_other._port;
+	//}
+
+	//bool NetworkClient::connect() 
+	//{
+	//	return _socket.connect(_addr, _port) == sf::Socket::Done;
+	//}
+
+	//void NetworkClient::disconnect() 
+	//{
+	//	_socket.disconnect();
+	//}
+
+	//void NetworkClient::setBlocking(const bool l_bool)
+	//{
+	//	_socket.setBlocking(l_bool);
+	//}
+	//
+	//bool NetworkClient::sendBytes(const char* l_bytes, const size_t l_length) 
+	//{
+	//	return _socket.send(l_bytes, l_length) == sf::Socket::Done;
+	//}
+
+	//bool NetworkClient::receiveBytes(char* l_buffer, const size_t l_maxBufferLength, size_t l_receivedBytes)
+	//{
+	//	return _socket.send(l_buffer, l_maxBufferLength, l_receivedBytes) == sf::Socket::Done;
+	//}
+
+}
