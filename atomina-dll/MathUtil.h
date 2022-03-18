347374114ca584ce86c055ee111357b6fd0f3172
Nate60 <natealdred60@gmail.com>
2022-03-18 09:00:51 -0400

    ATMA-20: Refactoring templated classes and functions for random generator and math util

--- atomina-dll/MathUtil.h
+++ atomina-dll/MathUtil.h
@@ -1,31 +1,23 @@
 #pragma once
 #include "AtominaException.h"
+#include <cmath>
 
-namespace ATMA {
+namespace ATMA
+{
 
-
-	/*
-	* Linear Interpolation between two values, with a given delta
-	* @param v0 Starting Boundary of interpolation
-	* @param v1 Ending Boundary of interpolation
-	* @param t The delta as a ratio of (v-v0)/v1-v0, must be between 0 and 1
-	*/
-	inline float lerp(float v0, float v1, float t) {
-		if (t > 1.0f || t < 0.0f)
-			throw InvalidParameterException("delta must be between 0.0f and 1.0f");
-		return (1 - t) * v0 + t * v1;
-	}
-
-
-	/*
-	* Overloaded Linear Interpolation between two values, with a given delta
-	* @param v0 Starting Boundary of interpolation
-	* @param v1 Ending Boundary of interpolation
-	* @param t The delta as a ratio of (v-v0)/v1-v0, must be between 0 and 1
-	*/
-	inline double lerp(double v0, double v1, double t) {
-		if (t > 1.0 || t < 0.0)
-			throw InvalidParameterException("delta must be between 0.0 and 1.0");
+/*
+* Linear Interpolation between two values, with a given delta
+* @param v0 Starting Boundary of interpolation
+* @param v1 Ending Boundary of interpolation
+* @param t The delta as a ratio of (v-v0)/v1-v0, must be between 0 and 1
+*/
+	template<class T>
+	T lerp(T v0, T v1, T t)
+	{
+		if(t > 1 || t < 0)
+		{
+			throw InvalidParameterException("delta must be between 0 and 1");
+		}
 		return (1 - t) * v0 + t * v1;
 	}
 
@@ -36,27 +28,14 @@ namespace ATMA {
 	* @param v1 Ending Boundary of interpolation
 	* @param t The delta as a ratio of (v-v0)/v1-v0, must be between 0 and 1
 	*/
-	inline float cubeTerp(float a0, float a1, float w) {
-		if (w > 1.0 || w < 0.0)
-			throw InvalidParameterException("delta must be between 0.0 and 1.0");
-		return (a1 - a0) * (3.0f - w * 2.0f) * w * w + a0;
+	template<class T>
+	T cubeTerp(T a0, T a1, T w)
+	{
+		if(w > 1 || w < 0)
+			throw InvalidParameterException("delta must be between 0 and 1");
+		return (a1 - a0) * (3 - w * 2) * w * w + a0;
 	}
 
-	/*
-	* Overloaded Cubic interpolation between two floats with a given delta, used for smoother stepping
-	* @param v0 Starting Boundary of interpolation
-	* @param v1 Ending Boundary of interpolation
-	* @param t The delta as a ratio of (v-v0)/v1-v0, must be between 0 and 1
-	*/
-	inline double cubeTerp(double a0, double a1, double w) {
-		if (w > 1.0 || w < 0.0)
-			throw InvalidParameterException("delta must be between 0.0 and 1.0");
-		return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
-	}
-
-
-
-	//function headers need to be exported
 
 	/*
 	* Used to generate perlin noise at a specific location on a gradient
@@ -65,25 +44,44 @@ namespace ATMA {
 	* @param l_x: x coordinate point to get the noise at
 	* @param l_y: y coordinate point to get the noise at
 	*/
-	ATMA_API double perlin(sf::Vector2<double> gradient[4], std::function<double (double, double, double) > interpolate, double l_x, double l_y);
+	template<class T>
+	T perlin(sf::Vector2<T> l_gradient[4], std::function<T(T, T, T)> l_lerp, T l_x, T l_y)
+	{
+		T a0 = std::floor(l_x);
+		T a1 = a0 + 1;
+		T b0 = std::floor(l_y);
+		T b1 = b0 + 1;
 
-	/*
-	* Used to generate perlin noise at a specific location on a gradient using floats
-	* @param gradient: Array of 4 directional vectors that define the gradient around the point
-	* @param interpolate: interpolation function to interpolate values in the gradient
-	* @param l_x: x coordinate point to get the noise at
-	* @param l_y: y coordinate point to get the noise at
-	*/
-	ATMA_API float perlin(sf::Vector2f gradient[4], std::function<float (float, float, float)> interpolate, float l_x, float l_y);
+		T dx = l_x - a0;
+		T dy = l_y - b0;
+
+		T temp1, temp2, inter1, inter2;
+
+		sf::Vector2<T> distanceVector = sf::Vector2<T>(dx, dy);
+		temp1 = l_gradient[0] * distanceVector;
+
+		distanceVector = sf::Vector2<T>(l_x - a1, dy);
+		temp2 = l_gradient[1] * distanceVector;
+		inter1 = l_lerp(temp1, temp2, dx);
+
+		distanceVector = sf::Vector2<T>(dx, l_y - b1);
+		temp1 = l_gradient[2] * distanceVector;
+
+		distanceVector = sf::Vector2<T>(l_x - a1, l_y - b1);
+		temp2 = l_gradient[3] * distanceVector;
+		inter2 = l_lerp(temp1, temp2, dx);
+
+		return l_lerp(inter1, inter2, dy);
+	}
+
 
 	//inline dot product for vectors
 
-#define VEC_DOT(vec_size,vec_type) operator * (sf::Vector##vec_size##vec_type a, sf::Vector##vec_size##vec_type b) { return a.x * b.x + a.y * b.y;}
-
-	inline double VEC_DOT(2, <double>)
-
-	inline float VEC_DOT(2, f)
-		
-	inline int VEC_DOT(2, i)
-
+	template<class T>
+	T operator * (sf::Vector2<T> a, sf::Vector2<T> b)
+	{
+		return a.x * b.x + a.y * b.y;
+	}
 }
+
+
