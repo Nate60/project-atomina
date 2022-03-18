9e8cdcd8052e0897cb1634aa1ab1d2f09f46887b
Nate60 <natealdred60@gmail.com>
2022-03-18 09:00:51 -0400

    ATMA-20: Refactoring templated classes and functions for random generator and math util

--- atomina-dll/RandomGenerator.cpp
+++ atomina-dll/RandomGenerator.cpp
@@ -4,64 +4,24 @@
 
 namespace ATMA {
 
-	//template instances for the accceptable types. Needed to be exported for visability
-	template ATMA_API class RandomGenerator<int>;
-	template ATMA_API class RandomGenerator<float>;
-	template ATMA_API class RandomGenerator<double>;
 
-	int RandomGenerator<int>::operator()(int l_min, int l_max) {
+	int RandomGenerator<int>::operator()(int l_min, int l_max)
+	{
 		return generate(l_min, l_max);
 	}
 
-	float RandomGenerator<float>::operator()(float l_min, float l_max) {
-		return generate(l_min, l_max);
-	}
+	int RandomGenerator<int>::generate(int l_min, int l_max)
+	{
+		std::lock_guard<std::mutex> l_lock{m_mtx};
 
-	double RandomGenerator<double>::operator()(double l_min, double l_max) {
-		return generate(l_min, l_max);
-	}
+		if(l_min > l_max) std::swap(l_min, l_max);
 
-	//explicit specialization requries an empty template: C2906
-	template <>
-	int RandomGenerator<int>::generate(int l_min, int l_max) {
-		mtx_.lock();
-
-		if (l_min > l_max) std::swap(l_min, l_max);
-
-		if (l_min != intDistribution_.min() || l_max != intDistribution_.max()) {
-			intDistribution_ = std::uniform_int_distribution<int>(l_min, l_max);
+		if(l_min != m_distribution.min() || l_max != m_distribution.max())
+		{
+			m_distribution = std::uniform_int_distribution<int>(l_min, l_max);
 		}
-		mtx_.unlock();
 
-		return intDistribution_(engine_);
-	}
-
-	
-	template <>
-	float RandomGenerator<float>::generate(float l_min, float l_max) {
-		mtx_.lock();
-
-		if (l_min > l_max) std::swap(l_min, l_max);
-
-		if (l_min != floatDistribution_.min() || l_max != floatDistribution_.max()) {
-			floatDistribution_ = std::uniform_real_distribution<float>(l_min, l_max);
-		}
-		mtx_.unlock();
-
-		return floatDistribution_(engine_);
-	}
-
-	template <>
-	double RandomGenerator<double>::generate(double l_min, double l_max) {
-		mtx_.lock();
-
-		if (l_min > l_max) std::swap(l_min, l_max);
-
-		if (l_min != doubleDistribution_.min() || l_max != doubleDistribution_.max()) {
-			doubleDistribution_ = std::uniform_real_distribution<double>(l_min, l_max);
-		}
-		mtx_.unlock();
-		return doubleDistribution_(engine_);
+		return m_distribution(m_engine);
 	}
 
 }
