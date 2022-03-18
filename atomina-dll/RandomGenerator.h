e47329007fe702df5b720e35d581e8550412f5a3
Nate60 <natealdred60@gmail.com>
2022-03-18 09:00:51 -0400

    ATMA-20: Refactoring templated classes and functions for random generator and math util

--- atomina-dll/RandomGenerator.h
+++ atomina-dll/RandomGenerator.h
@@ -13,24 +13,65 @@ namespace ATMA {
 	class ATMA_API RandomGenerator
 	{
 	private:
-		std::random_device device_;
-		std::mt19937 engine_;
-		std::uniform_int_distribution<int> intDistribution_;
-		std::uniform_real_distribution<float> floatDistribution_;
-		std::uniform_real_distribution<double> doubleDistribution_;
-		std::mutex mtx_;
+		std::random_device m_device;
+		std::mt19937 m_engine;
+		std::uniform_real_distribution<T> m_distribution;
+		std::mutex m_mtx;
 	public:
 
 		//default constructor
-		RandomGenerator() : engine_(device_()) {}
+		RandomGenerator() : m_engine(m_device()) 
+		{}
 
 		//calls the generate function with the given range
-		T operator()(T l_min, T l_max);
+		T operator()(T l_min, T l_max)
+		{
+			return generate(l_min, l_max);
+		}
+
 
 		//generates a random number between the given range inclusive
-		T generate(T l_min, T l_max);
+		T generate(T l_min, T l_max)
+		{
+			std::lock_guard<std::mutex> l_lock{m_mtx};
+
+			if(l_min > l_max) std::swap(l_min, l_max);
+
+			if(l_min != m_distribution.min() || l_max != m_distribution.max())
+			{
+				m_distribution = std::uniform_real_distribution<T>(l_min, l_max);
+			}
+
+			return m_distribution(m_engine);
+		}
 
 	};
+
+	//explicit template instantiation for support types
+	template class RandomGenerator<float>;
+	template class RandomGenerator<double>;
+
+	//explicit template specialization for int since it uses different distribution class
+	template<>
+	class ATMA_API RandomGenerator<int>
+	{
+	private:
+		std::random_device m_device;
+		std::mt19937 m_engine;
+		std::uniform_int_distribution<int> m_distribution;
+		std::mutex m_mtx;
+	public:
+				//default constructor
+		RandomGenerator(): m_engine(m_device())
+		{}
+
+        //calls the generate function with the given range
+		int operator()(int l_min, int l_max);
+
+		//generates a random number between the given range inclusive
+		int generate(int l_min, int l_max);
+	};
+
 }
 
 
