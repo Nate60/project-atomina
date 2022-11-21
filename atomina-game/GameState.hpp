05c00c5d769be774acdfe0d74562c888daa23047
nate60 <natealdred60@gmail.com>
2022-11-21 03:54:28 -0500

    ATMA-39: Properties Class added

--- atomina-game/GameState.hpp
+++ atomina-game/GameState.hpp
@@ -1,4 +1,11 @@
 #pragma once
+#include "OAS/Attribute.hpp"
+#include "OAS/attributes/AttrMouseRegion.hpp"
+#include "OAS/attributes/AttrMouseTrigger.hpp"
+#include "event/ObjectEvent.hpp"
+#include "event/ObjectEventContext.hpp"
+#include "util/Log.hpp"
+#include <SFML/System/Vector2.hpp>
 #include <functional>
 #include <atomina.hpp>
 
@@ -9,7 +16,27 @@ public:
 
     GameState(): BaseState() {}
 
-    virtual void onCreate() override {}
+    virtual void onCreate() override
+    {
+        // create button
+        auto butID = ctx.createObject();
+        ctx.addAttribute(butID, ATMA::AttributeType(ATMA::Attribute::MouseRegion));
+
+        auto region = ctx.getAttribute<ATMA::AttrMouseRegion>(
+            butID, ATMA::AttributeType(ATMA::Attribute::MouseRegion)
+        );
+        region->m_region.height = 100;
+        region->m_region.width = 100;
+        ctx.addAttribute(butID, ATMA::AttributeType(ATMA::Attribute::MouseTrigger));
+
+        auto trigger = ctx.getAttribute<ATMA::AttrMouseTrigger>(
+            butID, ATMA::AttributeType(ATMA::Attribute::MouseTrigger)
+        );
+        trigger->m_mouseReleaseFunc = [](const ATMA::ObjectEventContext &l_e) -> void
+        { ATMA_ENGINE_INFO("Button was PRESSED!!"); };
+
+        ctx.addAttribute(butID, ATMA::AttributeType(ATMA::Attribute::Graphic));
+    }
 
     virtual void onDestroy() override {}
 
@@ -29,6 +56,14 @@ public:
         {
             ctx.dispatchObjectEvent(ATMA::ObjectEventContext{
                 ATMA::ObjectEventType(ATMA::ObjectEvent::ShutDown)});
+        }
+        else if(e.type == sf::Event::MouseButtonReleased)
+        {
+            ATMA::Props eventProps{};
+            eventProps["mousepos"] =
+                sf::Vector2i{l_e.m_event.mouseButton.x, l_e.m_event.mouseButton.y};
+            ctx.dispatchObjectEvent(ATMA::ObjectEventContext{
+                ATMA::ObjectEventType(ATMA::ObjectEvent::MouseReleased), eventProps});
         }
     }
 
---
+++ atomina-test/util/PropsTest.cpp
@@ -0,0 +1,63 @@
+#include "PropsTest.hpp"
+#include "util/AtominaException.hpp"
+#include <gtest/gtest.h>
+
+using namespace std::string_literals;
+
+TEST(PropsTest, StoresValue)
+{
+    ATMA::Props props{};
+    props["key"] = "value"s;
+    EXPECT_TRUE(props.contains("key"));
+}
+
+TEST(PropsTest, StoreAndGetAsValue)
+{
+    ATMA::Props props{};
+    props["key"] = "value"s; // must be casted or it won't be the correct type
+    EXPECT_TRUE(props.contains("key"));
+    EXPECT_EQ("value"s, props.getAs<std::string>("key"));
+}
+
+TEST(PropsTest, GetAsNonExistentValueThrowsException)
+{
+    ATMA::Props props{};
+    EXPECT_THROW(props.getAs<std::string>("key"), ATMA::ValueNotFoundException);
+}
+
+TEST(PropsTest, StoreAndCheckValue)
+{
+    ATMA::Props props{};
+    props["key"] = "value"s;
+    EXPECT_TRUE(props.contains("key"));
+    EXPECT_EQ("value"s, std::any_cast<std::string>(props["key"]));
+}
+
+TEST(PropsTest, GetNonExistValueThrowsException)
+{
+    ATMA::Props props{};
+    EXPECT_THROW(props.remove("key"), ATMA::ValueNotFoundException);
+}
+
+TEST(PropsTest, RemoveValue)
+{
+    ATMA::Props props{};
+    props["key"] = "value"s;
+    EXPECT_TRUE(props.contains("key"));
+    props.remove("key");
+    EXPECT_FALSE(props.contains("key"));
+}
+
+TEST(PropsTest, IsCaseInsensitve)
+{
+    ATMA::Props props{};
+    props["key"] = "value"s;
+    EXPECT_TRUE(props.contains("KeY"));
+}
+
+TEST(PropsTest, CanBeCaseSensitve)
+{
+    ATMA::Props props{true};
+    props["key"] = "value"s;
+    EXPECT_FALSE(props.contains("KeY"));
+}
