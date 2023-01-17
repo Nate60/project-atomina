bab8377b679c1437e962932338f3162c14929b98
Nate60 <natealdred60@gmail.com>
2023-01-17 03:41:28 -0500

    ATMA-48: Setup Ninja build with MSVC

--- sbin/buildproject.bat
+++ sbin/buildproject.bat
@@ -6,7 +6,7 @@ cd "out"
 if not exist "%PROJECTPATH%\out\build" mkdir "build"
 cd "build"
 
-cmake "../../" -DCMAKE_BUILD_TYPE=%1 -DCMAKE_CONFIGURATION_TYPES=%1 -DCMAKE_EXPORT_COMPILE_COMMANDS=True %*
+cmake "../../" -DCMAKE_BUILD_TYPE=%1 -DCMAKE_CONFIGURATION_TYPES=%1 %*
 cmake --build . --config %1
 
 cd "%CURRENTPATH%"
