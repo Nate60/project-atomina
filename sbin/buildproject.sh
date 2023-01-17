eec50d5be453833415c2c5cb3a447c6f707e430d
Nate60 <natealdred60@gmail.com>
2023-01-17 03:41:28 -0500

    ATMA-48: Setup Ninja build with MSVC

--- sbin/buildproject.sh
+++ sbin/buildproject.sh
@@ -1,6 +1,6 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH/../
 mkdir -p out/build && cd out/build
-cmake ../../ -DCMAKE_CXX_FLAGS="-fpic" -DCMAKE_EXPORT_COMPILE_COMMANDS=True -DCMAKE_BUILD_TYPE=$1 -DCMAKE_CONFIGURATION_TYPES=$1 ${@:2} && cmake --build .
+cmake ../../ -DCMAKE_CXX_FLAGS="-fpic" -DCMAKE_EXPORT_COMPILE_COMMANDS=True -DCMAKE_BUILD_TYPE=$1 -DCMAKE_CONFIGURATION_TYPES=$1 ${@:2} && cmake --build . --config $1
 cp $SCRIPTPATH/../out/build/compile_commands.json $SCRIPTPATH/../
 
