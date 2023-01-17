b639c62a97362a1aea198419ce28bb32b28b4edb
Nate60 <natealdred60@gmail.com>
2023-01-17 03:41:28 -0500

    ATMA-48: Setup Ninja build with MSVC

--- sbin/rungame.sh
+++ sbin/rungame.sh
@@ -1,7 +1,7 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH/../
-GTESTPATH=$SCRIPTPATH/../out/build/lib/$1/
-SPDLOGPATH=$SCRIPTPATH/../out/build/_deps/spdlog-build/$1/
-SFMLPATH=$SCRIPTPATH/../out/build/_deps/sfml-build/lib/$1/
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GTESTPATH:$SPDLOGPATH:$SFMLPATH
+#GTESTPATH=$SCRIPTPATH/../out/build/lib/$1/
+#SPDLOGPATH=$SCRIPTPATH/../out/build/_deps/spdlog-build/$1/
+#SFMLPATH=$SCRIPTPATH/../out/build/_deps/sfml-build/lib/$1/
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GTESTPATH:$SPDLOGPATH:$SFMLPATH
 xvfb-run ./out/build/bin/Atomina_Game
