3df8bbec3b0b665c1385004cea34ce4e2451adf3
Nate60 <natealdred60@gmail.com>
2023-01-19 01:03:25 -0500

    ATMA-49: Updated readme

--- sbin/runtests.sh
+++ sbin/runtests.sh
@@ -4,4 +4,4 @@ cd $SCRIPTPATH/../
 #SPDLOGPATH=$SCRIPTPATH/../out/build/_deps/spdlog-build/$1/
 #SFMLPATH=$SCRIPTPATH/../out/build/_deps/sfml-build/lib/$1/
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GTESTPATH:$SPDLOGPATH:$SFMLPATH
-xvfb-run ./out/build/bin/Atomina_Test
+xvfb-run -a ./out/build/bin/Atomina_Test
