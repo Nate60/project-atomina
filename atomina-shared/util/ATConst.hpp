793ce81ad66008e4550a995369698a8af9ffc1e6
Nate60 <natealdred60@gmail.com>
2023-01-22 00:46:12 -0500

    ATMA-51: removed casting warnings

--- atomina-shared/util/ATConst.hpp
+++ atomina-shared/util/ATConst.hpp
@@ -6,36 +6,24 @@
 namespace ATConst
 {
 
-    /**
-     * Default image that consideres of magenta and black pixels tiled
-     * to be used as a fall back for when the expected image
-     * cannot be found
-     * @returns Image of checkered pixels 
-     */
-    static const sf::Image &getDefaultImage()
-    {
-        ATMA_ENGINE_INFO("Default Image is being used");
-        static sf::Image img;
-        static const sf::Uint8 pixels[16]{
-            0xFF,
-            0x00,
-            0xFF,
-            0xFF,
-            0x00,
-            0x00,
-            0x00,
-            0xFF,
-            0x00,
-            0x00,
-            0x00,
-            0xFF,
-            0xFF,
-            0x00,
-            0xFF,
-            0xFF};
-        img.create({2u, 2u}, pixels);
-        return img;
-    }
+    static const sf::Uint8 DEFAULT_PIXELS[16]{
+        0xFF,
+        0x00,
+        0xFF,
+        0xFF,
+        0x00,
+        0x00,
+        0x00,
+        0xFF,
+        0x00,
+        0x00,
+        0x00,
+        0xFF,
+        0xFF,
+        0x00,
+        0xFF,
+        0xFF};
+
 
     static const size_t OBJECT_BIT_SIZE = 32;
 
