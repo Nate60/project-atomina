e635bf71cc456bc13026fc4b6b5e0ba6dbed8a58
Nate60 <natealdred60@gmail.com>
2023-07-15 02:31:10 -0400

    ATMA-66: Text rendering is its own system

--- atomina-shared/core/Game.cpp
+++ atomina-shared/core/Game.cpp
@@ -21,11 +21,13 @@ namespace ATMA
         ctx.registerAttributeType<AttrVelocity>(AttributeType(Attribute::Velocity));
         ctx.registerAttributeType<AttrSprite>(AttributeType(Attribute::Sprite));
         ctx.registerAttributeType<AttrRenderable>(AttributeType(Attribute::Renderable));
+        ctx.registerAttributeType<AttrText>(AttributeType(Attribute::Text));
 
         // system registration
         ctx.addSystemType<SysController>(SystemType(System::Controller));
         ctx.addSystemType<SysTranslator>(SystemType(System::Translator));
         ctx.addSystemType<SysRenderer>(SystemType(System::Renderer));
+        ctx.addSystemType<SysTextRenderer>(SystemType(System::TextRenderer));
     }
 
     void Game::run()
