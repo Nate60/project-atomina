9ecde0db81b61b9b74b776115cb2fe40d8674064
Nate60 <natealdred60@gmail.com>
2023-08-03 02:33:27 -0400

    ATMA-70: Initialize glew for both windows and linux

--- atomina-shared/util/ATConst.hpp
+++ atomina-shared/util/ATConst.hpp
@@ -8,4 +8,62 @@ namespace ATConst
 
     static const size_t OBJECT_BIT_SIZE = 32;
 
+#ifdef _WINDOWS
+    const PIXELFORMATDESCRIPTOR DEFAULT_PIXEL_FORMAT_D = {
+        sizeof(PIXELFORMATDESCRIPTOR),
+        1,
+        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER, // Flags
+        PFD_TYPE_RGBA, // The kind of framebuffer. RGBA or palette.
+        32,            // Colordepth of the framebuffer.
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        24, // Number of bits for the depthbuffer
+        8,  // Number of bits for the stencilbuffer
+        0,  // Number of Aux buffers in the framebuffer.
+        PFD_MAIN_PLANE,
+        0,
+        0,
+        0,
+        0};
+#elif linux
+    const int DEFAULT_VISUAL_ATTRIBUTES[] = {
+        GLX_X_RENDERABLE,
+        True,
+        GLX_DRAWABLE_TYPE,
+        GLX_WINDOW_BIT,
+        GLX_RENDER_TYPE,
+        GLX_RGBA_BIT,
+        GLX_X_VISUAL_TYPE,
+        GLX_TRUE_COLOR,
+        GLX_RED_SIZE,
+        8,
+        GLX_GREEN_SIZE,
+        8,
+        GLX_BLUE_SIZE,
+        8,
+        GLX_ALPHA_SIZE,
+        8,
+        GLX_DEPTH_SIZE,
+        24,
+        GLX_STENCIL_SIZE,
+        8,
+        GLX_DOUBLEBUFFER,
+        True,
+        GLX_SAMPLE_BUFFERS,
+        1,
+        GLX_SAMPLES,
+        4,
+        0L};
+#endif
 }
